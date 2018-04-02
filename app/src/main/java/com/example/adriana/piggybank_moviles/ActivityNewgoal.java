22365e37f8c08b9cad0f42a42fa58ab4697aeb8d
adriana zarate zarate <adrianazarate0397@gmail.com>
2018-04-02 08:57:15 -0500

    Activities[01]

---
+++ app/src/main/java/com/example/adriana/piggybank_moviles/ActivityNewgoal.java
@@ -0,0 +1,18 @@
+package com.example.adriana.piggybank_moviles;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by adriana on 02/04/2018.
+ */
+
+public class ActivityNewgoal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newgoal);
+    }
+
+}
