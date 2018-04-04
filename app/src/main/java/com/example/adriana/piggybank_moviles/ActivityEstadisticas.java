59103dc18daedce7e92a31a906b553f848df7c3d
adriana zarate zarate <adrianazarate0397@gmail.com>
2018-04-03 19:41:23 -0500

    Pantallas de adriana [01]

---
+++ app/src/main/java/com/example/adriana/piggybank_moviles/ActivityEstadisticas.java
@@ -0,0 +1,59 @@
+package com.example.adriana.piggybank_moviles;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ActivityEstadisticas extends AppCompatActivity {
+
+//    private Toolbar toolbar;
+//    private TabLayout tabLayout;
+//    private ViewPager viewPager;
+
+ //   SectionsPagerAdapter mSectionsPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estadisticas);
+
+//        toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+//        viewPager = (ViewPager) findViewById(R.id.viewpager);
+//        viewPager.setAdapter(mSectionsPagerAdapter);
+//        tabLayout = (TabLayout) findViewById(R.id.tabs);
+//        tabLayout.setupWithViewPager(viewPager);
+    }
+
+//    public static class PlaceholderFragment extends Fragment {
+//
+//        private static final String ARG_SECTION_NUMBER = "section_number";
+//
+//        public static PlaceholderFragment newInstance(int sectionNumber) {
+//            PlaceholderFragment fragment = new PlaceholderFragment();
+//            Bundle args = new Bundle();
+//            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+//            fragment.setArguments(args);
+//            return fragment;
+//        }
+//
+//        public PlaceholderFragment() {
+//        }
+//
+//        @Override
+//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                                 Bundle savedInstanceState) {
+//            View rootView = inflater.inflate(R.layout.fragment_activity_estadisticas, container, false);
+//            return rootView;
+//        }
+//    }
+
+}
